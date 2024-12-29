35aded44708017e2cbee13c80d4e1b1472b930e1
Gabriel Prata <119508139+gabrielmprata@users.noreply.github.com>
2024-12-29 19:35:19 -0300

    Add files via upload

--- viking_evol.py
+++ viking_evol.py
@@ -61,24 +61,115 @@ peso_ant = (df_inbody_full[['Valor']]
 
 var_peso = (peso.Valor.values[0] - peso_ant.Valor.values[0]).round(2)
 
+# Massa de gordura
+gordura = (df_inbody_full[['Valor']]
+           [(df_inbody_full['ano_mes'] == maxi) & (df_inbody_full['kpi'] == 4)]
+           )
+
+gordura_ant = (df_inbody_full[['Valor']]
+               [(df_inbody_full['ano_mes'] == mes_ant)
+                & (df_inbody_full['kpi'] == 4)]
+               )
+
+var_gordura = (gordura.Valor.values[0] - gordura_ant.Valor.values[0]).round(2)
+
+# Massa Muscular Esquelética
+massa = (df_inbody_full[['Valor']]
+         [(df_inbody_full['ano_mes'] == maxi) & (df_inbody_full['kpi'] == 6)]
+         )
+
+massa_ant = (df_inbody_full[['Valor']]
+             [(df_inbody_full['ano_mes'] == mes_ant)
+              & (df_inbody_full['kpi'] == 6)]
+             )
+
+var_massa = (massa.Valor.values[0] - massa_ant.Valor.values[0]).round(2)
+
+# Porcentual de gordura
+pgc = (df_inbody_full[['Valor']]
+       [(df_inbody_full['ano_mes'] == maxi) & (df_inbody_full['kpi'] == 8)]
+       )
+
+pgc_ant = (df_inbody_full[['Valor']]
+           [(df_inbody_full['ano_mes'] == mes_ant) & (df_inbody_full['kpi'] == 8)]
+           )
+
+var_pgc = (pgc.Valor.values[0] - pgc_ant.Valor.values[0]).round(2)
+
+# ----------------------------------------------------------------------------#
+# 2. Análise músculo gordura
+df_inbody_full_gr = (df_inbody_full[['ano_mes', 'Metrica', 'kpi', 'Valor']]).query(
+    'kpi in (4, 5, 6) ')
+
+# ----------------------------------------------------------------------------#
+# 3. Porcentual de gordura
+df_inbody_pgc = (
+    df_inbody_full[['ano_mes', 'Metrica', 'kpi', 'Valor']]).query('kpi == 8')
+
 
 #######################
 # Construção dos Gráficos
 
 # 1. Avaliação
-# Peso feito com st.metric direto no Main Panel
+# todos os cards foram feitos com st.metric direto no Main Panel
+
+# ----------------------------------------------------------------------------#
+# 2. Análise músculo gordura
+hist = px.line(df_inbody_full_gr, x='ano_mes', y='Valor', color='Metrica',
+               markers=True, text='Valor',
+               # height=600, width=800, #altura x largura
+               line_shape="spline",
+               template="plotly_dark",
+               render_mode="svg",
+               color_discrete_sequence=["blue", "green", "red"],
+               category_orders={"Metrica": [
+                   "Peso", "Massa Muscular Esquelética", "Massa de gordura"]},
+               labels=dict(ano_mes="Ano e mês",
+                           Valor="Kg", variable="Métrica")
+               )
+
+hist.update_layout(legend=dict(
+    yanchor="top",
+    y=-0.3,
+    xanchor="left",
+    x=0.01))
+# se o type for date, vai respeitar o intervalo
+hist.update_xaxes(type="category")
+hist.update_traces(line_width=2, textposition='top center')
+
+# -------------------------------------------------------#
+# 3 Porcentual de Gordura
+gr_pcg = px.line(df_inbody_pgc, x='ano_mes', y='Valor', color='Metrica',
+                 markers=True, text='Valor',
+                 height=150, width=800,  # altura x largura
+                 line_shape="spline",
+                 template="plotly_dark",
+                 render_mode="svg",
+                 color_discrete_sequence=["orange"],
+
+                 labels=dict(ano_mes="Ano e mês",
+                             Valor="%", variable="Métrica")
+                 )
+# se o type for date, vai respeitar o intervalo
+gr_pcg.update_xaxes(type="category")
+gr_pcg.update_layout(margin=dict(t=1, b=0, l=0, r=0), showlegend=False)
+gr_pcg.update_traces(line_width=2, textposition='top center')
 
 
 #######################
 # Dashboard Main Panel
 
-st.markdown("# :crossed_swords: Análise das medidas :hammer_and_pick:")
+st.image("https://raw.githubusercontent.com/gabrielmprata/inbody_bio/main/img/Header_Viking3.jpg")
+st.markdown("# :crossed_swords: Viking Evolution :hammer_and_pick:")
 
 st.write(":blue[Avaliação:]", maxi, "	:date:")
 
+style_metric_cards(background_color="#071021",
+                   border_left_color="#1f66bd", border_radius_px=5)
+
 with st.expander("Analise Músculo-Gordura", expanded=True):
 
-    col = st.columns((1.1, 1.1, 1.1), gap='medium')
+    col = st.columns((1.1, 1.1, 1.1, 1.1), gap='medium')
 
     with col[0]:
         #######################
@@ -89,9 +180,21 @@ with st.expander("Analise Músculo-Gordura", expanded=True):
 
     with col[1]:
         st.markdown('### Massa Magra')
+        st.metric(label="", value=str(
+            (massa.Valor.values[0]).round(2)), delta=str(var_massa))
 
     with col[2]:
-        st.markdown('### Massa de Gordura')
+        st.markdown('### Massa Gorda')
+        st.metric(delta_color="inverse", label="", value=str(
+            (gordura.Valor.values[0]).round(2)), delta=str(var_gordura))
 
-style_metric_cards(background_color="#071021",
-                   border_left_color="#1f66bd", border_radius_px=5)
+    with col[3]:
+        st.markdown('### % Gordura')
+        st.metric(delta_color="inverse", label="", value=str(
+            (pgc.Valor.values[0]).round(2)), delta=str(var_pgc))
+
+with st.expander("Histórico", expanded=True):
+    st.plotly_chart(hist, use_container_width=True)
+
+with st.expander("Histórico do Porcentual de Gordura", expanded=True):
+    st.plotly_chart(gr_pcg, use_container_width=True)
